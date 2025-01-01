e322059235e5a3cd1355246755993394ba20f4ff
Mathys-Gasnier <mathys35.gasnier@gmail.com>
2025-01-01 13:39:04 +0100

    Revert "Making the language folder name, the name of the language instead of the slug name of the language"
    This reverts commit 63eaf1909a5cda96ff93ed05e3367edea1100339.

--- utils/consolidateSnippets.js
+++ utils/consolidateSnippets.js
@@ -1,5 +1,5 @@
 import { exit } from 'process';
-import { parseAllSnippets, slugify } from './snippetParser.js';
+import { parseAllSnippets, reverseSlugify } from './snippetParser.js';
 import { join } from 'path';
 import { copyFileSync, writeFileSync } from 'fs';
 
@@ -14,14 +14,13 @@ if(errored) exit(1);
 
 const index = [];
 for(const [language, categories] of Object.entries(snippets)) {
-    const languageSlugName = slugify(language);
     const languageIconPath = join(snippetsPath, language, 'icon.svg');
 
-    copyFileSync(languageIconPath, join(iconPath, `${languageSlugName}.svg`));
+    copyFileSync(languageIconPath, join(iconPath, `${language}.svg`));
 
-    index.push({ lang: language, icon: `/icons/${languageSlugName}.svg` });
+    index.push({ lang: reverseSlugify(language), icon: `/icons/${language}.svg` });
     
-    const languageFilePath = join(dataPath, `${languageSlugName}.json`);
+    const languageFilePath = join(dataPath, `${language}.json`);
     
     writeFileSync(languageFilePath, JSON.stringify(categories, null, 4));
 }
--- utils/snippetParser.js
+++ utils/snippetParser.js
@@ -8,17 +8,6 @@ export function reverseSlugify(string, separator = "-") {
         .join(' ')
         .trim();
 }
-export function slugify(string, separator = "-") {
-    return string
-        .toString() // Cast to string (optional)
-        .toLowerCase() // Convert the string to lowercase letters
-        .trim() // Remove whitespace from both sides of a string (optional)
-        .replace(/\s+/g, separator) // Replace spaces with {separator}
-        .replace(/[^\w\-]+/g, "") // Remove all non-word chars
-        .replace(/\_/g, separator) // Replace _ with {separator}
-        .replace(/\-\-+/g, separator) // Replace multiple - with single {separator}
-        .replace(/\-$/g, ""); // Remove trailing -
-}
 
 let errored = false;
 function raise(issue, snippet = '') {
