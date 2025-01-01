63eaf1909a5cda96ff93ed05e3367edea1100339
Mathys-Gasnier <mathys35.gasnier@gmail.com>
2025-01-01 13:32:12 +0100

    Making the language folder name, the name of the language instead of the slug name of the language

--- utils/consolidateSnippets.js
+++ utils/consolidateSnippets.js
@@ -1,5 +1,5 @@
 import { exit } from 'process';
-import { parseAllSnippets, reverseSlugify } from './snippetParser.js';
+import { parseAllSnippets, slugify } from './snippetParser.js';
 import { join } from 'path';
 import { copyFileSync, writeFileSync } from 'fs';
 
@@ -14,13 +14,14 @@ if(errored) exit(1);
 
 const index = [];
 for(const [language, categories] of Object.entries(snippets)) {
+    const languageSlugName = slugify(language);
     const languageIconPath = join(snippetsPath, language, 'icon.svg');
 
-    copyFileSync(languageIconPath, join(iconPath, `${language}.svg`));
+    copyFileSync(languageIconPath, join(iconPath, `${languageSlugName}.svg`));
 
-    index.push({ lang: reverseSlugify(language), icon: `/icons/${language}.svg` });
+    index.push({ lang: language, icon: `/icons/${languageSlugName}.svg` });
     
-    const languageFilePath = join(dataPath, `${language}.json`);
+    const languageFilePath = join(dataPath, `${languageSlugName}.json`);
     
     writeFileSync(languageFilePath, JSON.stringify(categories, null, 4));
 }
