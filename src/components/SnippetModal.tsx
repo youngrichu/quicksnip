98d69c05039b83c763ac4d6b81052677793280bd
MAYUR <mayuresh1377@gmail.com>
2025-01-01 15:00:08 +0530

    fix: add exit animations

--- src/components/SnippetModal.tsx
+++ src/components/SnippetModal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Button from "./Button";
 import { CloseIcon } from "./Icons";
 import CodePreview from "./CodePreview";
